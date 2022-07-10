4c7ed5d67b23bd6c53a9c57076516ae702fd55b0
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-07-10 19:46:30 -0400

    passing list items in the body

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   var today = new Date();
   console.log(today.toString());
@@ -14,7 +15,10 @@ app.get("/", (req, res) => {
 
   res.render("list", { typeOfDay: day });
 });
-
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  console.log(item);
+});
 app.listen(3000, () => {
   console.log("server is running");
 });
