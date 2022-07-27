54233897ba7cdb9dcbba735f7760fa8eda165d34
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-07-27 19:05:49 -0400

    adding styles

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
-var items = [];
+let items = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  var today = new Date();
+  let today = new Date();
   console.log(today.toString());
   var options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { typeOfDay: day, items: items });
 });
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
