2a2b1bff022ab9d6d7cba777bd3373df2d72f36c
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-07-30 09:45:05 -0400

    css styling

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 let items = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("Public"));
 app.get("/", (req, res) => {
   let today = new Date();
   console.log(today.toString());
@@ -20,7 +22,7 @@ app.post("/", function (req, res) {
   items.push(item);
   res.redirect("/");
 });
-
+app.use("/css", express.static(path.resolve(__dirname, "Public/css")));
 app.listen(3000, () => {
   console.log("server is running");
 });
