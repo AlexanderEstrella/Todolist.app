850cca26316321c3046c1ba22b4dbb4ee1a92248
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-08-24 22:15:23 -0400

    passing date throught data.ejs

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-let items = [];
-let workItems = [];
+const date = require(__dirname + "/date.js");
+
+const items = [];
+const workItems = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("Public"));
 app.get("/", (req, res) => {
-  let today = new Date();
-  console.log(today.toString());
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDate();
   res.render("list", { ListTitle: day, newListItems: items });
 });
 app.post("/", function (req, res) {
