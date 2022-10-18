0c5b3fd5c6d3481b6fce4e9d89d5b3b64a066d7c
AlexanderEstrella <alexanderestrella23@gmail.com>
2022-10-18 18:40:03 -0400

    created delete option, which deletes from our database as well

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const { stringify } = require("querystring");
 const date = require("./date");
 
 app.set("view engine", "ejs");
@@ -29,6 +30,13 @@ const item3 = new item({
 });
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
 //inserting the received items to our database if request not received - default get sent
 app.get("/", (req, res) => {
   let day = date.getDate();
@@ -47,6 +55,15 @@ app.get("/", (req, res) => {
     }
   });
 });
+
+app.get("/:any", (req, res) => {
+  const ovar = req.params.any;
+  const list = new List({
+    name: ovar,
+    items: defaultItems,
+  });
+  list.save();
+});
 // adding new items to database/home route
 app.post("/", function (req, res) {
   const itemName = req.body.newItem;
@@ -55,6 +72,18 @@ app.post("/", function (req, res) {
   });
   Item.save();
   res.redirect("/");
+});
+// deleting items from out database
+app.post("/delete", (req, res) => {
+  const checkedItemid = req.body.checkeditem;
+  item.findOneAndDelete({ _id: checkedItemid }, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("item was deleted");
+      res.redirect("/");
+    }
+  });
 });
 
 app.use("/css", express.static(path.resolve(__dirname, "Public/css")));
